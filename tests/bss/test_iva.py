e83e284fdd8a5e01df42476df5913e11a8fd3d7a
tky823 <tky823.main+github@gmail.com>
2022-04-21 08:37:27 +0900

    [test] fix path error

--- tests/bss/test_iva.py
+++ tests/bss/test_iva.py
@@ -1,4 +1,5 @@
 import numpy as np
+from scipy.io import loadmat
 
 
 def _set_seed(seed=42):
@@ -10,9 +11,9 @@ def _convolve_rir(reverb=0.160, intervals=[8, 8, 8, 8, 8, 8, 8], degree=[0]):
     "(Reverberation_{reverb:.3f}s)_{intervals}_1m_{degree:03d}.mat"
 
     rir_path = rir_path.format(reverb=reverb, intervals=intervals, degree=degree)
-    # rir_mat = loadmat(rir_path)
-    # rir = rir_mat["impulse_response"]
-    # print(rir.shape)
+    rir_mat = loadmat(rir_path)
+    rir = rir_mat["impulse_response"]
+    print(rir.shape)
     print(rir_path)
 
 
