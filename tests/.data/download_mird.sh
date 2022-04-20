e9baf0db5561dbf7b1621e3815187cdd9a6645f5
tky823 <tky823.main+github@gmail.com>
2022-04-21 08:25:10 +0900

    [test] add unzip

--- tests/.data/download_mird.sh
+++ tests/.data/download_mird.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
 filename="Impulse_response_Acoustic_Lab_Bar-Ilan_University__Reverberation_0.160s__8-8-8-8-8-8-8.zip"
-wget -q -P "./tests/.data/${filename}" "https://www.iks.rwth-aachen.de/fileadmin/user_upload/downloads/forschung/tools-downloads/${filename}"
+
+wget -q -P "./tests/.data/" "https://www.iks.rwth-aachen.de/fileadmin/user_upload/downloads/forschung/tools-downloads/${filename}"
+unzip -d "./tests/.data/" "./tests/.data/${filename}"
