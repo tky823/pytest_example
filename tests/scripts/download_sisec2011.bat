16813e09181aa4abfcb4e65a7fae1b3adb4f8341
tky823 <tky823.main+github@gmail.com>
2022-04-21 21:40:10 +0900

    [test] fix typo

--- tests/scripts/download_sisec2011.bat
+++ tests/scripts/download_sisec2011.bat
@@ -1,7 +1,7 @@
 @echo off
 
-set root="./tests/.data"
-set filename="dev1.zip"
+set root=./tests/.data
+set filename=dev1.zip
 
 echo %root%/%filename%
 bitsadmin /transfer "Download" "http://www.irisa.fr/metiss/SiSEC10/underdetermined/dev1.zip" %root%/%filename%
