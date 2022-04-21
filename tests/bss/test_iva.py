cc3934090780e58fe5d8ad0fdce403433048c37f
tky823 <tky823.main+github@gmail.com>
2022-04-21 09:18:09 +0900

    fix format string

--- tests/bss/test_iva.py
+++ tests/bss/test_iva.py
@@ -12,12 +12,16 @@ def _convolve_rir(reverb=0.160, intervals=[8, 8, 8, 8, 8, 8, 8], degree=[0]):
         "(Reverberation_{reverb:.3f}s)_{intervals}_1m_{degree:03d}.mat"
     )
 
-    rir_path = rir_path.format(reverb=reverb, intervals=intervals, degree=degree)
+    rir_path = rir_path.format(
+        reverb=reverb, intervals="-".join(intervals), degree=degree
+    )
+
+    print(rir_path)
+
     rir_mat = loadmat(rir_path)
     rir = rir_mat["impulse_response"]
 
     print(rir.shape)
-    print(rir_path)
 
 
 if __name__ == "__main__":
