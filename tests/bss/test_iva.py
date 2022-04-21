a50af303b4b005b245ee42062d885a65108054f7
tky823 <tky823.main+github@gmail.com>
2022-04-21 09:25:22 +0900

    [test] fix argument

--- tests/bss/test_iva.py
+++ tests/bss/test_iva.py
@@ -6,7 +6,7 @@ def _set_seed(seed=42):
     np.random.seed(seed)
 
 
-def _convolve_rir(reverb=0.160, intervals=[8, 8, 8, 8, 8, 8, 8], degree=[0]):
+def _convolve_rir(reverb=0.160, intervals=[8, 8, 8, 8, 8, 8, 8], degree=0):
     rir_path = (
         "./tests/.data/Impulse_response_Acoustic_Lab_Bar-Ilan_University_"
         "(Reverberation_{reverb:.3f}s)_{intervals}_1m_{degree:03d}.mat"
