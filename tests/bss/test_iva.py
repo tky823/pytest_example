fc89feb43996b4e09a3c8c502cc7925114a17013
tky823 <tky823.main+github@gmail.com>
2022-04-21 09:13:55 +0900

    [test] fix path

--- tests/bss/test_iva.py
+++ tests/bss/test_iva.py
@@ -7,12 +7,15 @@ def _set_seed(seed=42):
 
 
 def _convolve_rir(reverb=0.160, intervals=[8, 8, 8, 8, 8, 8, 8], degree=[0]):
-    rir_path = "./tests/.data/Impulse_response_Acoustic_Lab_Bar-Ilan_University_"
-    "(Reverberation_{reverb:.3f}s)_{intervals}_1m_{degree:03d}.mat"
+    rir_path = (
+        "./tests/.data/Impulse_response_Acoustic_Lab_Bar-Ilan_University_"
+        "(Reverberation_{reverb:.3f}s)_{intervals}_1m_{degree:03d}.mat"
+    )
 
     rir_path = rir_path.format(reverb=reverb, intervals=intervals, degree=degree)
     rir_mat = loadmat(rir_path)
     rir = rir_mat["impulse_response"]
+
     print(rir.shape)
     print(rir_path)
 
