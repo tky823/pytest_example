ea154911f74a9b41d102563a476d18a09742bb49
tky823 <tky823.main+github@gmail.com>
2022-04-21 22:13:54 +0900

    [test] fix filepath

--- tests/scripts/download_sisec2011.bat
+++ tests/scripts/download_sisec2011.bat
@@ -4,5 +4,6 @@ set root=tests\.data
 set filename=dev1.zip
 
 mkdir %root%
-bitsadmin /transfer "Download" http://www.irisa.fr/metiss/SiSEC10/underdetermined/dev1.zip %CD%\%root%\%filename%
-dir %root%
+bitsadmin /transfer "Download" http://www.irisa.fr/metiss/SiSEC10/underdetermined/%filename% %CD%\%root%\%filename%
+call powershell -command "Expand-Archive %root%\%filename%"
+dir
