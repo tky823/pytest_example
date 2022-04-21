b9570b9300f132a672e3a175c69c1f2196ffe6f3
tky823 <tky823.main+github@gmail.com>
2022-04-21 21:53:39 +0900

    [test] add pwd for windows

--- tests/scripts/download_sisec2011.bat
+++ tests/scripts/download_sisec2011.bat
@@ -4,6 +4,6 @@ set root=./tests/.data
 set filename=dev1.zip
 
 echo %root%/%filename%
-echo @cd
+@cd
 bitsadmin /transfer "Download" http://www.irisa.fr/metiss/SiSEC10/underdetermined/dev1.zip %root%/%filename%
 dir
