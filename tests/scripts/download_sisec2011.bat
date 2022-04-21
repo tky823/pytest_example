95489338307ca202d41cb79e4a5864a67e721dec
tky823 <tky823.main+github@gmail.com>
2022-04-21 21:33:53 +0900

    [test] add echo

--- tests/scripts/download_sisec2011.bat
+++ tests/scripts/download_sisec2011.bat
@@ -3,5 +3,6 @@
 set root="./tests/.data"
 set filename="dev1.zip"
 
+echo "%root%/%filename%"
 bitsadmin /transfer "Download" "http://www.irisa.fr/metiss/SiSEC10/underdetermined/dev1.zip" "%root%/%filename%"
 dir
