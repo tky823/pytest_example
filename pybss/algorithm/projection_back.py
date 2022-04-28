379f7b995f1d62dfefed6f5ee634ff6dd4cd03b2
tky823 <tky823.main+github@gmail.com>
2022-04-29 01:09:53 +0900

    [refactor] change max-length of line

--- pybss/algorithm/projection_back.py
+++ pybss/algorithm/projection_back.py
@@ -9,14 +9,10 @@ def projection_back(input, reference):
     if n_dims == 2:
         # Y: (n_sources, n_bins, n_frames)
         # X: (1, n_bins, n_frames)
-        X = reference[np.newaxis, :, :].transpose(
-            1, 0, 2
-        )  # (n_bins, n_channels, n_frames)
+        X = reference[np.newaxis, :, :].transpose(1, 0, 2)  # (n_bins, n_channels, n_frames)
         Y = Y.transpose(1, 0, 2)  # (n_bins, n_sources, n_frames)
         Y_Hermite = Y.transpose(0, 2, 1).conj()  # (n_bins, n_frames, n_sources)
-        YY_Hermite_inverse = np.linalg.inv(
-            Y @ Y_Hermite
-        )  # (n_bins, n_sources, n_sources)
+        YY_Hermite_inverse = np.linalg.inv(Y @ Y_Hermite)  # (n_bins, n_sources, n_sources)
         A = X @ Y_Hermite @ YY_Hermite_inverse  # (n_bins, n_channels, n_sources)
         scale = A[:, 0, :].transpose(1, 0)  # (n_sources, n_bins)
     elif n_dims == 3:
@@ -25,14 +21,10 @@ def projection_back(input, reference):
         X = reference.transpose(1, 0, 2)  # (n_bins, n_channels, n_frames)
         Y = Y.transpose(1, 0, 2)  # (n_bins, n_sources, n_frames)
         Y_Hermite = Y.transpose(0, 2, 1).conj()  # (n_bins, n_frames, n_sources)
-        YY_Hermite_inverse = np.linalg.inv(
-            Y @ Y_Hermite
-        )  # (n_bins, n_sources, n_sources)
+        YY_Hermite_inverse = np.linalg.inv(Y @ Y_Hermite)  # (n_bins, n_sources, n_sources)
         A = X @ Y_Hermite @ YY_Hermite_inverse  # (n_bins, n_channels, n_sources)
         scale = A.transpose(1, 2, 0)  # (n_channels, n_sources, n_bins)
     else:
-        raise ValueError(
-            "reference.ndim is expected 2 or 3, but given {}.".format(n_dims)
-        )
+        raise ValueError("reference.ndim is expected 2 or 3, but given {}.".format(n_dims))
 
     return scale
