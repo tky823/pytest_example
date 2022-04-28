e4450e313880e0e0c1219682862cd4703981fda1
tky823 <tky823.main+github@gmail.com>
2022-04-29 00:08:18 +0900

    [chore] add setup.cfg

--- setup.py
+++ setup.py
@@ -1,10 +1,3 @@
-from setuptools import setup, find_packages
+from setuptools import setup
 
-setup(
-    name="pybss",
-    version="0.0.0",
-    url="https://github.com/tky823/pytest_example",
-    packages=find_packages(exclude=["tests"]),
-    python_requires=">=3.7, <4",
-    install_requires=["numpy"],
-)
+setup()
